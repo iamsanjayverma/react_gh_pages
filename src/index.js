11aa1d0085ac1551e151fba2cc494c6ec2e3c4f9
iamsanjayverma <139140046+iamsanjayverma@users.noreply.github.com>
2023-12-21 20:49:02 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import Home from '../src/Pages/Home/index'
+import Contact from '../src/Pages/Contact/index'
+import Login from '../src/Pages/Login/index'
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<Home />);
+root.render(
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </BrowserRouter>
+);
