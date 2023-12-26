f65e484ac4e23d9a2c8815df2742bbe5f1fcbb81
iamsanjayverma <139140046+iamsanjayverma@users.noreply.github.com>
2023-12-26 23:18:30 +0530

    update pages

---
+++ src/Pages/index.js
@@ -0,0 +1,20 @@
+// Example App.js with routes
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home/index'
+import About from './Login/index';
+import Contact from './Contact/index';
+
+function index() {
+  return ( <BrowserRouter>
+    <Routes>
+    <Route path="/" exact component={Home} />
+        <Route path="/login" component={About} />
+        <Route path="/contact" component={Contact} />
+    </Routes>
+  </BrowserRouter>
+
+  );
+}
+
+export default index;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import Home from '../src/Pages/Home/index'
+import App from '../src/Pages/index'
 
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<Home />);
+root.render(<App />);
