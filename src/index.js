e2ed819ee29e941fd31ba5bf4599bccbb5670b78
iamsanjayverma <139140046+iamsanjayverma@users.noreply.github.com>
2023-12-26 23:25:49 +0530

    update pages

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 import App from '../src/Pages/index'
+import { HashRouter } from 'react-router-dom';
 
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(  <HashRouter>
+    <App />
+  </HashRouter>);
