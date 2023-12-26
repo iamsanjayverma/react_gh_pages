3fd9dce2d21b4ce58ce8cf68f98d22f8de542f98
iamsanjayverma <139140046+iamsanjayverma@users.noreply.github.com>
2023-12-26 23:51:08 +0530

    update pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom/client';
-import App from '../src/Pages/Home/index'
-
+import App from '../src/Pages/index'
 
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(  <App />);
+root.render(<App />);
